475d5546fa61c84b4d2dfee1e48dfe9c766f08d7
Brijesh Singh <brijesh.singh1@stellantis.com>
2025-05-13 17:41:40 +0530

    comment

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
-
+//ggggg
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
